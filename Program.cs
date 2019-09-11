29d5780b8d62a50d33c2bfdb082effd803d41ec0
jstam <jstam009@gmail.com>
2019-09-10 21:03:37 -0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Week3Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many years do you have in professional programming?");
+
+            try
+            {
+                String practiceYears = Console.ReadLine(); ;
+
+                int inputYears = int.Parse(practiceYears);
+
+                if (inputYears == 0)
+                {
+                    Console.WriteLine("You need a lot of practice.");
+                }
+                else if (inputYears == 1)
+                {
+                    Console.WriteLine("Looks like you have a little experience.");
+                }
+                else if (inputYears == 2)
+                {
+                    Console.WriteLine("Wow! You've been doing this for a little while.");
+                }
+                else 
+                 {
+                    Console.WriteLine("Youre an Expert!");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("You didnt input any years!");
+            }
+        }
+    }
+}
